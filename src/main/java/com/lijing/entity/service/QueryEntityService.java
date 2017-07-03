f74841678c9af04f0db9bc1e1c63a2d4863bb526
lijing <jing_li@xinyan.com>
2017-07-03 18:52:10 +0800

    update

--- src/main/java/com/lijing/entity/service/QueryEntityService.java
+++ src/main/java/com/lijing/entity/service/QueryEntityService.java
@@ -32,24 +32,60 @@ public class QueryEntityService {
 
     private List<EntityInfo> queryEntity(String dbName,List<String> tableNameList){
         if(tableNameList==null||tableNameList.isEmpty()){
-            List<TableInfoDto> tableInfoDtoList = tableColumnInfoMapper.selectTable(dbName, null);
-            if(tableInfoDtoList!=null&&!tableInfoDtoList.isEmpty()){
-                for (TableInfoDto tableInfoDto:tableInfoDtoList){
-                    List<ColumnInfoDto> columnInfoDtoList = tableColumnInfoMapper.selectColumn(dbName, tableInfoDto.getTableName());
-                }
-
-            }
+            return queryAllByDB(dbName);
         }else {
-            List<TableInfoDto> tableInfoDtoList = new ArrayList<TableInfoDto>();
-            for (String tableName : tableNameList){
-                List<TableInfoDto> list = tableColumnInfoMapper.selectTable(dbName, tableName);
-                if(list!=null&&list.size()==1){
-                    tableInfoDtoList.add(list.get(0));
-                }
+            return queryTableInfo(dbName, tableNameList);
+        }
+    }
+
+    /**
+     * 根据数据库名称，查询该库中所有表信息
+     * @param dbName 数据库名称
+     * @return 表信息
+     */
+    private List<EntityInfo> queryAllByDB(String dbName){
+        List<EntityInfo> result = new ArrayList<EntityInfo>();
+        List<TableInfoDto> tableInfoDtoList = tableColumnInfoMapper.selectTable(dbName, null);
+        if(tableInfoDtoList!=null&&!tableInfoDtoList.isEmpty()){
+            for (TableInfoDto tableInfoDto:tableInfoDtoList){
+                result.add(queryColumnInfo(dbName,tableInfoDto));
             }
         }
+        return result;
+    }
 
+    /**
+     * 根据数据库名称和表名查询表信息
+     * @param dbName 数据库名称
+     * @param tableNameList 表名称集合
+     * @return 表信息
+     */
+    private List<EntityInfo> queryTableInfo(String dbName,List<String> tableNameList){
+        List<EntityInfo> result = new ArrayList<EntityInfo>();
+        for (String tableName : tableNameList){
+            List<TableInfoDto> list = tableColumnInfoMapper.selectTable(dbName, tableName);
+            if(list!=null&&list.size()==1){
+                TableInfoDto tableInfoDto = list.get(0);
+                result.add(queryColumnInfo(dbName,tableInfoDto));
+            }
+        }
+    }
 
+    /**
+     * 根据数据库名称和表信息查询该表的列信息
+     * @param dbName 数据库名称
+     * @param tableInfoDto 表信息
+     * @return 该表列信息
+     */
+    private EntityInfo queryColumnInfo(String dbName,TableInfoDto tableInfoDto){
+        List<ColumnInfoDto> columnInfoDtoList = tableColumnInfoMapper.selectColumn(dbName, tableInfoDto.getTableName());
+        if(columnInfoDtoList!=null) {
+            EntityInfo entityInfo = new EntityInfo();
+            entityInfo.setTableInfoDto(tableInfoDto);
+            entityInfo.setColumnInfoDtoList(columnInfoDtoList);
+            return entityInfo;
+        }
+        return null;
     }
 
 }
